17522f8e87bffb54d8a566815da76ff0d0eb5942
donghyun <semaj9310@gmail.com>
2023-10-08 21:07:05 +0900

    Ch 8 - install chart library and basic ui
    Signed-off-by: donghyun <semaj9310@gmail.com>

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -1,4 +1,5 @@
 defmodule PentoWeb.Admin.SurveyResultsLive do
+  alias Contex.Plot
   use PentoWeb, :live_component
 
   alias Pento.Catalog
@@ -7,11 +8,62 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign_products_with_average_ratings()}
+     |> assign_products_with_average_ratings()
+     |> assign_dataset()
+     |> assign_chart()
+     |> assign_chart_svg()}
   end
 
   def assign_products_with_average_ratings(socket) do
     socket
     |> assign(:products_with_average_ratings, Catalog.products_with_average_ratings())
+  end
+
+  def assign_dataset(
+        %{assigns: %{products_with_average_ratings: products_with_average_ratings}} = socket
+      ) do
+    socket
+    |> assign(:dataset, make_bar_chart_dataset(products_with_average_ratings))
+  end
+
+  defp make_bar_chart_dataset(data) do
+    Contex.Dataset.new(data)
+  end
+
+  defp assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+    socket
+    |> assign(:chart, make_bar_chart(dataset))
+  end
+
+  defp make_bar_chart(dataset) do
+    Contex.BarChart.new(dataset)
+  end
+
+  def assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
+    socket
+    |> assign(:chart_svg, render_bar_chart(chart))
+  end
+
+  defp render_bar_chart(chart) do
+    Plot.new(500, 400, chart)
+    |> Plot.titles(title(), subtitle())
+    |> Plot.axis_labels(x_axis(), y_axis())
+    |> Plot.to_svg()
+  end
+
+  defp title do
+    "Product Ratings"
+  end
+
+  defp subtitle do
+    "average star ratings per product"
+  end
+
+  defp x_axis do
+    "products"
+  end
+
+  defp y_axis do
+    "stars"
   end
 end

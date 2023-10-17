a9b946b6dbfc8ffd38d2a022f41d9b16c057bfe5
donghyun <semaj9310@gmail.com>
2023-10-17 23:25:33 +0900

    Ch 11 - Bulid the Game Core

---
+++ lib/pento/game/pentomino.ex
@@ -0,0 +1,42 @@
+defmodule Pento.Game.Pentomino do
+  alias Pento.Game.Point
+  alias Pento.Game.Shape
+
+  @names [:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]
+  @default_location {8, 8}
+
+  defstruct name: :i,
+            rotation: 0,
+            reflected: false,
+            location: @default_location
+
+  def new(fields \\ []), do: __struct__(fields)
+
+  def rotate(%{rotation: degrees} = p) do
+    %{p | rotation: rem(degrees + 90, 360)}
+  end
+
+  def flip(%{reflected: reflection} = p) do
+    %{p | reflected: not reflection}
+  end
+
+  def up(p) do
+    %{p | location: Point.move(p.location, {0, -1})}
+  end
+
+  def down(p) do
+    %{p | location: Point.move(p.location, {0, 1})}
+  end
+
+  def left(p) do
+    %{p | location: Point.move(p.location, {-1, 0})}
+  end
+
+  def right(p) do
+    %{p | location: Point.move(p.location, {1, 0})}
+  end
+
+  def to_shape(pento) do
+    Shape.new(pento.name, pento.rotation, pento.reflected, pento.location)
+  end
+end
